65f9382a50746ad6bf116903cc389d38b5faa041
Kevin Heidorn <128178416+Kevin-Heidorn@users.noreply.github.com>
2024-05-08 15:46:03 -0300

    Update 6.js

--- AtividadePraxis/6.js
+++ AtividadePraxis/6.js
@@ -1,23 +1,28 @@
-// Exercicio 6
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 function Afilme(idade, tingresso) {
-    if (idade >= 18) {
-      return true;
-    } else if (idade >= 16 && tingresso.toLowerCase() === "meia") {
-      return true;
-    } else {
-      return false;
-    }
-  }
-  
-  const idade = parseInt(prompt("Qual é a sua idade?"));
-  const tingresso = prompt("Qual é o tipo de ingresso? (Inteira/Meia)").toLowerCase();
-  
-  if (Afilme(idade, tingresso)) {
-    console.log("Você pode assistir ao filme!");
+  if (idade >= 18) {
+    return true;
+  } else if (idade >= 16 && tingresso.toLowerCase() === "meia") {
+    return true;
   } else {
-    console.log("Você não pode assistir ao filme.");
+    return false;
   }
-  
+}
 
-
-
+rl.question("Qual é a sua idade? ", (idadeInput) => {
+  const idade = parseInt(idadeInput);
+  rl.question("Qual é o tipo de ingresso? (Inteira/Meia) ", (tingresso) => {
+    if (Afilme(idade, tingresso)) {
+      console.log("Você pode assistir ao filme!");
+    } else {
+      console.log("Você não pode assistir ao filme.");
+    }
+    rl.close();
+  });
+});
