aad6070f500ead97f4260b303442c1ac55c24717
Kevin Heidorn <128178416+Kevin-Heidorn@users.noreply.github.com>
2024-05-08 15:34:13 -0300

    Update 7.js

--- AtividadePraxis/7.js
+++ AtividadePraxis/7.js
@@ -1,34 +1,51 @@
-// Exercicio 7
+const readline = require('readline');
 
-let array = []; 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-while (true) {
-  const operacao = parseInt(prompt(`Escolha uma operação:
+let array = [];
+
+function promptUser() {
+  rl.question(`Escolha uma operação:
 1: Adicionar um elemento no final do array.
 2: Remover o último elemento do array.
 3: Remover o primeiro elemento do array.
 4: Exibir todos os elementos do array.
-0: Sair do programa.`));
-
-  switch (operacao) {
-    case 1:
-      const elemento = prompt("Digite o elemento a ser adicionado:");
-      array.push(elemento);
-      break;
-    case 2:
-      array.pop();
-      break;
-    case 3:
-      array.shift();
-      break;
-    case 4:
-      console.log(array);
-      break;
-    case 0:
-      console.log("Programa encerrado.");
-      process.exit(); 
-    default:
-      console.log("Operação inválida. Tente novamente.");
-  }
+0: Sair do programa.
+`, (answer) => {
+    const operacao = parseInt(answer);
+    switch (operacao) {
+      case 1:
+        rl.question("Digite o elemento a ser adicionado: ", (elemento) => {
+          array.push(elemento);
+          promptUser();
+        });
+        break;
+      case 2:
+        array.pop();
+        promptUser();
+        break;
+      case 3:
+        array.shift();
+        promptUser();
+        break;
+      case 4:
+        console.log(array);
+        promptUser();
+        break;
+      case 0:
+        console.log("Programa encerrado.");
+        rl.close();
+        break;
+      default:
+        console.log("Operação inválida. Tente novamente.");
+        promptUser();
+    }
+  });
 }
+
+promptUser();
+
 
